5bd0e4dd8edfae3de4f7f3a0d966c56e43f5d47b
caldardn <caldardn@gmail.com>
2023-10-24 12:04:48 -0400

    adding markdown js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== None) {
+    return `[Static Badge](https://img.shields.io/badge/${license}-content)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `https://img.shields.io/badge/License-${license}-blue.svg`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `\n\n## License\n\nThis application uses the ${license} License.`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -34,14 +52,15 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## License 
-  The license is:${data.license}
+  The license is: ${renderLicenseSection(data.license)}
+  This is the link: ${renderLicenseLink(data.license)}
 
   ## Contribution 
   ${data.contribution}
 
   ## Tests 
   Use ${data.test} to run tests
-  
+
   ## Questions 
   * For any other questions about the application please contact me by emailing: ${data.email}
   * To see other applications developed by me please visit my GitHub profile: [${data.github}](https://github.com/${data.github}/)
